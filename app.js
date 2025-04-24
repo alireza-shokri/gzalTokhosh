97fabf7b8fcc4b3ad44c513f170bd129ca6dab29
alireza <lshkry88@gmail.com>
2025-04-24 17:29:16 +0330

    'improv'

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
+const boxes = document.querySelectorAll('.card-pattern');
+const menuIcon = document.querySelector('.menu_icon');
+const menuList = document.getElementById('menu_list');
+const landing_page=document.getElementById('landing_page')
+
+
+  menuIcon.addEventListener('click', () => {
+    menuList.classList.toggle('hidden');
+  });
+
+
+
+
+
+
+
+
+
+
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+       observer.unobserve(entry.target); 
+    }
+    else   entry.target.classList.remove('active');
+  });
+}, {
+  threshold: 0.5
+});
+
+boxes.forEach(box => observer.observe(box));
+
+
+
+  setTimeout(() => {
+  landing_page.classList.add('hidden')
+  }, 4000);
+
+
+
 window.addEventListener('scroll', function () {
     const scrollY = window.scrollY; // مقدار اسکرول عمودی
     const target = document.getElementById('floating_btn');
